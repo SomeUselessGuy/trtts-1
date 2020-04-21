d38aa8d9f08797b40c1f6a93f9573a2fd30c323d
Useless guy <40398890+SomeUselessGuy@users.noreply.github.com>
2020-04-21 18:40:04 +0500

    Теперь у нас кубик сломанный есть :3

--- pj_20200414/main.cc
+++ pj_20200414/main.cc
@@ -1,18 +1,35 @@
+/*
+ * main.cc
+ *
+ *  Created on: 14 апр. 2020 г.
+ *      Author: unyuu
+ */
 
+#include "globals.h"
 
+#include "MyWindow.h"
 
+#include <SDL2/SDL_main.h>
 
+int main(int, char **)
+{
+	try {
+		SDL_Init(SDL_INIT_EVERYTHING);
 
+		MyWindow win;
 
+		win.main_loop();
+	} catch (const std::exception & e) {
+		std::cerr <<
+				"При выполнении программы возникла нештатная ситуация:" <<
+				std::endl << e.what() << std::endl;
+		return 1;
+	} catch (...) {
+		std::cerr <<
+				"При выполнении программы возникла "
+				"неизвестная нештатная ситуация." << std::endl;
+		return 2;
+	}
 
-
-
-#include <iostream>
-
-#include <GL/gl.h>
-#include <GL/glu.h>
-
-int main(){
-	std::cout << "ja ded!" << std::endl;
 	return 0;
 }
